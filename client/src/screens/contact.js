54fb17378f2c578a8ae14675a85cbe2bc30e475a
gloria ojuks <gloriaojuks@gmail.com>
2019-03-25 14:43:44 +0100

    contact form

--- client/src/screens/contact.js
+++ client/src/screens/contact.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 import './contact.css'
 
 
@@ -8,13 +9,28 @@ class Contact extends Component {
         return(
             <div style={{textAlign: "center"}}>
                 <h1 style={{color: "green", marginBottom: "30px"}}>Contact Us Here</h1>
+                <Form>
 
-                <form>
-                 <input className="form_field" type="text" placeholder="full name" /><br />
-                 <input className="form_field" type="textA" placeholder="email address" /><br />
-                 <textarea cols="111" rows="10" placeholder="leave a message" /><br />
-                 <button style={{backgroundColor: "#ccc", with: "100%"}}>submit</button>
-                </form>
+        <FormGroup>
+          <Label for="exampleEmail">First Name</Label>
+          <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+        </FormGroup>
+       
+        <FormGroup>
+          <Label for="exampleEmail">Email</Label>
+          <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+        </FormGroup>
+       
+       
+        <FormGroup>
+          <Label for="exampleText">Text Area</Label>
+          <Input type="textarea" name="text" id="exampleText" />
+        </FormGroup>
+    
+        
+        <Button>Submit</Button>
+      </Form>
+                
             </div>
         )
     }
