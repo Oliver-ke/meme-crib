7aa55c8410de3c2a24e9b8024bf4a4a09b9a7d1e
gloria ojuks <gloriaojuks@gmail.com>
2019-04-05 13:14:05 +0100

    image and css updates

--- client/src/screens/About.js
+++ client/src/screens/About.js
@@ -1,11 +1,21 @@
 import React, {Component} from 'react'
+import './about.css'
+import about_banner from '../assets/images/about_banner.jpg';
+
 
 
 class About extends Component {
     render() {
         return(
             <div>
-                hello from about
+              
+                    <img className="about_banner" src={about_banner} />
+                    <div className="info_section">
+                    <h4>WE BRING YOU FUN</h4>
+
+                    </div>
+                           
+                 
             </div>
         )
     }
